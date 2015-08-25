21a0e0bdf9a12d005819b3380a14d67ee4d75ff3
Nate Bird <natebird@gmail.com>
2015-08-25 14:33:46 -0700

    Add fragment list view using recycler view

--- app/src/main/java/co/natebird/criminalintent/CrimeListActivity.java
+++ app/src/main/java/co/natebird/criminalintent/CrimeListActivity.java
@@ -2,10 +2,9 @@ package co.natebird.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
-
 }
