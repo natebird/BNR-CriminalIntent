0837519f8ddec57f2a0f52cb0bdb096c9c2fe025
Nate Bird <natebird@gmail.com>
2015-08-25 15:57:06 -0700

    Cleanup unused imports.

--- app/src/main/java/co/natebird/criminalintent/CrimeListFragment.java
+++ app/src/main/java/co/natebird/criminalintent/CrimeListFragment.java
@@ -10,8 +10,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.List;
 
 public class CrimeListFragment extends Fragment {
